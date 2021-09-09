2084b8489211cbcf788a3d92b819af9c2a4caab9
grx <grx0917@sina.com>
2021-09-09 09:29:54 +0800

    Initial Commit

---
+++ GGJiePaiTools/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GGJiePaiTools
+//
+//  Created by GRX on 2021/9/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GGJiePaiTools/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GGJiePaiTools
+//
+//  Created by GRX on 2021/9/9.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
